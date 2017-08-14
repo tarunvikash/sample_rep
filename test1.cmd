d7f50b30c3192ff6c8e9daa3bfe98a96b344ae84
tarunvikash <tarunvikash123@gmail.com>
2017-08-14 15:50:03 +0530

    Update test1.cmd

--- test1.cmd
+++ test1.cmd
@@ -8,3 +8,4 @@ for /f "skip=1 tokens=2-4 delims=(-)" %%a in ('echo/^|date') do (
    for /f "tokens=%t%-4 delims=.-/ " %%d in ('date/t') do ( 
       set %%a=%%d&set %%b=%%e&set %%c=%%f)) 
 endlocal&set %1=%yy%&set %2=%mm%&set %3=%dd%&goto :EOF
+C:\strawberry\perl\bin\perl test1.pl
